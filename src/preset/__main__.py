814a8fb42de0e2f7f6e46263275e48722df2257d
Andrey Gubarev <andrey@andreygubarev.com>
2024-11-23 19:13:05 +0400

    replaced environment print with argparse for ansible paths

--- src/preset/__main__.py
+++ src/preset/__main__.py
@@ -1,5 +1,16 @@
 import os
-import requests
+import argparse
 
-for key, value in os.environ.items():
-    print(f"{key}={value}")
+WORKDIR = os.environ['WORKDIR']
+USERDIR = os.environ['USER_PWD']
+
+def main():
+    parser = argparse.ArgumentParser(description='Preset')
+    parser.add_argument('ansible_collection', type=str, help='Path to the Ansible Collection')
+    parser.add_argument('ansible_playbook', type=str, help='Path to the Ansible Playbook')
+    args = parser.parse_args()
+    print(args.ansible_collection)
+    print(args.ansible_playbook)
+
+if __name__ == '__main__':
+    main()
