c53b91a16f7617c1e2464e65c9e2ecfab0a7bd4e
Andrey Gubarev <andrey@andreygubarev.com>
2024-11-23 14:46:09 +0400

    added ansible home, roles and collections path configuration to entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -25,5 +25,26 @@ fi
 # ensure no pyc files
 export PYTHONDONTWRITEBYTECODE=1
 
+### environment | ansible #####################################################
+ANSIBLE_HOME="${ANSIBLE_HOME:-$WORKDIR/.ansible}"
+export ANSIBLE_HOME
+mkdir -p "$ANSIBLE_HOME"
+
+if [ -n "${ANSIBLE_ROLES_PATH:-}" ]; then
+    ANSIBLE_ROLES_PATH="$ANSIBLE_HOME/roles:$ANSIBLE_ROLES_PATH"
+else
+    ANSIBLE_ROLES_PATH="$ANSIBLE_HOME/roles"
+fi
+mkdir -p "$ANSIBLE_HOME/roles"
+export ANSIBLE_ROLES_PATH
+
+if [ -n "${ANSIBLE_COLLECTIONS_PATH:-}" ]; then
+    ANSIBLE_COLLECTIONS_PATH="$ANSIBLE_HOME/collections:$ANSIBLE_COLLECTIONS_PATH"
+else
+    ANSIBLE_COLLECTIONS_PATH="$ANSIBLE_HOME/collections"
+fi
+mkdir -p "$ANSIBLE_HOME/collections"
+export ANSIBLE_COLLECTIONS_PATH
+
 ### main ######################################################################
 "${PYTHONBIN}/python3" -m preset "$@"
